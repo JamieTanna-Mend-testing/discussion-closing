15fae9a1de44cfa4dbb77802db9d987fff69070f
Jamie Tanna <jamie.tanna@mend.io>
2025-11-14 13:48:26 +0000

    sq

--- .github/workflows/close-answered-discussions/script.js
+++ .github/workflows/close-answered-discussions/script.js
@@ -1,29 +1,21 @@
 // Parses the ISO date string and checks it's past the age window
 // Co-authored-by: GPT-4.1 (GitHub Copilot)
 function isOlderThanDaysAgo(dateString, daysAgo) {
-  return true  // HACK
-  // const parsedDate = new Date(dateString);
-  // const now = new Date();
-  // const thresholdDate = new Date(now.getTime() - daysAgo * 24 * 60 * 60 * 1000);
-  // console.log(thresholdDate)
-  // console.log(thresholdDate, parsedDate)
-  // return parsedDate < thresholdDate;
+  const parsedDate = new Date(dateString);
+  const now = new Date();
+  const thresholdDate = new Date(now.getTime() - daysAgo * 24 * 60 * 60 * 1000);
+  return parsedDate < thresholdDate;
 }
 
 module.exports = async ({ github, context, discussionAnsweredDays }) => {
-  // const { owner, repo } = context.repo()
-  owner = 'JamieTanna-Mend-testing'
-  repo = 'discussion-closing'
-
-  // // HACK
-  // owner = 'renovatebot'
-  // repo = 'renovate'
+  const owner = context.repo.owner
+  const repo = context.repo.repo
 
   let cursor = null
 
   const query = `query ($cursor: String) {
   repository(owner: "${owner}", name: "${repo}") {
-    discussions(after: $cursor, states: OPEN, answered: true, first: 1, orderBy: {field: CREATED_AT, direction: ASC}) {
+    discussions(after: $cursor, states: OPEN, answered: true, first: 50, orderBy: {field: CREATED_AT, direction: ASC}) {
       pageInfo {
         endCursor
         hasNextPage
@@ -38,25 +30,11 @@ module.exports = async ({ github, context, discussionAnsweredDays }) => {
   }
 }`
 
-  // let discussions = []
-
-  // NOTE not paginating https://github.com/actions/github-script/issues/309
   while (true) {
     console.debug({ cursor }, "Starting query")
-    const resp = await github.graphql(query, { cursor })
-    console.log({ resp })
-    const { repository } = resp
-    console.debug({ a: repository })
-    console.debug({ a: repository.discussions })
-    console.debug({ a: repository.discussions.edges })
+    const { repository } = await github.graphql(query, { cursor })
 
-    console.log(`Found ${repository.discussions.edges.length} discussions`);
-
-    // discussions.push(...repository.discussions.edges)
-
-    if (!resp.repository.discussions.pageInfo.hasNextPage) {
-      break
-    }
+    console.debug(`Found ${repository.discussions.edges.length} discussions in this page of data`);
 
     let numMutating = 0
     let mutation = 'mutation {'
@@ -80,11 +58,13 @@ module.exports = async ({ github, context, discussionAnsweredDays }) => {
     } else {
       console.debug(`Did not find any Discussions to close in this page of data`)
     }
+
+    if (!resp.repository.discussions.pageInfo.hasNextPage) {
+      break
+    }
+
     cursor = resp.repository.discussions.pageInfo.endCursor
   }
-
-  //
-  // console.log(`Found ${discussions.length} discussions!`);
 
   return ''
 }
